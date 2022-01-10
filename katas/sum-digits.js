f064c712888cb215da28aff25a296858f14a8f41
Jwilss3 <jessicalaurenwilson1@gmail.com>
2022-01-10 14:16:34 +0000

    Add filter method to sumDigits function

--- katas/sum-digits.js
+++ katas/sum-digits.js
@@ -10,7 +10,10 @@
 function sumDigits(num) {
   const digits = num.toString().split('');
   console.log(digits);
-  const digitsNum = digits.map(element => Number(element));
+  const removePeriod = digits.filter(digit => {if (digit != "."){
+    return true
+  }}) 
+  const digitsNum = removePeriod.map(element => Number(element));
   const sum = digitsNum.reduce((pv, cv) => pv + cv);
   return sum;
 }
